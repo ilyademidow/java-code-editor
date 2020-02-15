4755ea258103986d65cd201a0051d45472e0fbc2
Ilya Demidov <ilyademidow@mail.ru>
2020-02-15 23:08:33 +0300

    add username

--- src/App.js
+++ src/App.js
@@ -7,13 +7,23 @@ import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-github";
 
 const SERVER_URL = 'http://localhost:8080/api/v1/';
+const TMP_SAVE_CODE_INTERVAL = 5000;
 
 class App extends React.Component {
+  tmpUsername = "";
   defVal = "class Test {" + '\u000a' + " public static void main(String[] args) {" + '\u000a\u000a' + " }" + '\u000a' + "}";
 
   constructor(props) {
     super(props);
-    this.state = { output: { success: "", error: "" }, input: this.defVal };
+    this.state = {
+      tmpUsername: "",
+      username: "",
+      output: {
+        success: "",
+        error: ""
+      },
+      input: this.defVal
+    };
     this.saveTmpCodeSchedule();
   }
 
@@ -25,23 +35,21 @@ class App extends React.Component {
 
   saveTmpCodeSchedule = () => {
     console.log('save tmp ' + this.state.input);
-    axios.post(SERVER_URL + 'save_tmp', {"code": this.state.input}, {
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-    setTimeout(this.saveTmpCodeSchedule, 5000);
+    if ("" != this.state.username) {
+      axios.post(SERVER_URL + 'save_tmp', { "username": this.state.username, "code": this.state.input }, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+    }
+    setTimeout(this.saveTmpCodeSchedule, TMP_SAVE_CODE_INTERVAL);
   }
 
   send = () => {
-    // axios
-    //   .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    //   .then(response => (this.setState(response)))
-    //   .catch(resp => console.error(resp));
     this.setState({ output: { success: "compiling...", error: "" }, input: this.state.input });
 
     axios
-      .post(SERVER_URL + 'run', { "code": this.state.input.replace(this.regPlus, "&plus;") }, {
+      .post(SERVER_URL + 'run', { "username": this.state.username, "code": this.state.input.replace(this.regPlus, "&plus;") }, {
         headers: {
           'Content-Type': 'application/json',
         }
@@ -56,38 +64,62 @@ class App extends React.Component {
       });
   }
 
+  updateTmpUsername = (event) => {
+    this.setState({ tmpUsername: event.target.value });
+
+    console.log(this.state.tmpUsername);
+  }
+
+  setUsername = () => {
+    this.setState({ username: this.state.tmpUsername });
+    console.log(this.state.username);
+  }
+
   // Render editor
   render() {
-    return (
-      <div className="container-fluid">
-        <h2>Live code interview</h2>
-        <div className="row">
-          <div className="col-md-6 wrap-border">
-            For DB usage please use this parameters <b>Driver</b> <code>org.h2.Driver</code> <b>DB URL</b><code>jdbc:h2:~/test</code>
-        <AceEditor
-              width="100%"
-              mode="java"
-              theme="github"
-              fontSize="16px"
-              wrapEnabled={true}
-              defaultValue={this.defVal}
-              value={this.state.input}
-              onChange={this.onChange}
-              name="UNIQUE_ID_OF_DIV"
-              editorProps={{ $blockScrolling: true }}
-            />
+    if ("" === this.state.username) {
+      return (
+        <div className="container-fluid">
+          <div className="col-md-6">
+            <input type="text" id="username" onChange={this.updateTmpUsername} value={this.state.tmpUsername} />
           </div>
-          <div className="col-md-6 wrap-border">
-            Output:<br />
-            <pre>
-              {this.state.output.success}<br />
-              {this.state.output.error}<br />
-            </pre>
+          <div className="col-md-6">
+            <button className="btn btn-danger" onClick={this.setUsername}>Confirm and run editor</button>
           </div>
         </div>
-        <button className="btn btn-danger" onClick={this.send}>Run my code</button>
-      </div>
-    );
+      )
+    } else {
+      return (
+        <div className="container-fluid">
+          <h2>Live code interview</h2>
+          <div className="row">
+            <div className="col-md-6 wrap-border">
+              For DB usage please use this parameters <b>Driver</b> <code>org.h2.Driver</code> <b>DB URL</b><code>jdbc:h2:~/test</code>
+              <AceEditor
+                width="100%"
+                mode="java"
+                theme="github"
+                fontSize="16px"
+                wrapEnabled={true}
+                defaultValue={this.defVal}
+                value={this.state.input}
+                onChange={this.onChange}
+                name="UNIQUE_ID_OF_DIV"
+                editorProps={{ $blockScrolling: true }}
+              />
+            </div>
+            <div className="col-md-6 wrap-border">
+              Output:<br />
+              <pre>
+                {this.state.output.success}<br />
+                {this.state.output.error}<br />
+              </pre>
+            </div>
+          </div>
+          <button className="btn btn-danger" onClick={this.send}>Run my code</button>
+        </div>
+      );
+    }
   }
 }
 
